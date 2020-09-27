f012c208456763b29094df037191316d0a6e2e10
Trapti Singhal <trapti.singhal@in.ibm.com>
2020-09-27 23:21:34 +0530

    updated java version

--- testproject/src/main/java/com/example/testproject/controller/HelloWorldController.java
+++ testproject/src/main/java/com/example/testproject/controller/HelloWorldController.java
@@ -1,20 +1,23 @@
 package com.example.testproject.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.testproject.service.HelloWorldService;
 
 
-@Controller
+@RestController
 public class HelloWorldController {
 
+	@Autowired
 	HelloWorldService serv;
 	
 	@GetMapping("/hello")
 	public String sayHello()
 	{
-	  return serv.sayHello();
+		return serv.sayHello();
 	}
 	
 }
--- testproject/src/main/java/com/example/testproject/service/HelloWorldService.java
+++ testproject/src/main/java/com/example/testproject/service/HelloWorldService.java
@@ -1,5 +1,8 @@
 package com.example.testproject.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HelloWorldService {
 
 	public String sayHello() {
